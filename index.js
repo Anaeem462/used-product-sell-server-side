e9f6c7dd7fe5da41310dd6e09406132955b10a6a
sifa43 <sifatunnoor@gmail.com>
2022-11-24 18:03:21 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 //all requires
 const express = require("express");
-const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+//function call
+const app = express();
 const port = process.env.PORT || 5000;
 
 // all middleware
 app.use(cors());
 app.unsubscribe(express.json());
 
+// mongodb connect
+const uri = process.env.DB_URL;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const secondSellDb = client.db("2nd-Sell");
+const userCollection = secondSellDb.collection("users");
 //listener
 app.get("/", (req, res) => {
     res.send("2nd sell server");
 });
 
 app.listen(port, () => {
-    console.log("server is running on ", port);
+    client.connect((err) => {
+        // console.log(err);
+    });
+    console.log("server is running with mongodb on ", port);
 });
