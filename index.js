8976807cfadc5ba8221809675fb4b028507edf01
sifa43 <sifatunnoor@gmail.com>
2022-11-24 18:13:51 +0600

    json web token adde

--- index.js
+++ index.js
@@ -13,11 +13,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.unsubscribe(express.json());
 
+//verify user with jwt token
+
+function verifyJwt(req, res, next) {
+    const userTokens = req.headers.authorization;
+    if (!userTokens) {
+        return res.status(401).send("Unauthorized access");
+    }
+    jwt.verify(userTokens, process.env.USER_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send("unauthorized user");
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 // mongodb connect
 const uri = process.env.DB_URL;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const secondSellDb = client.db("2nd-Sell");
 const userCollection = secondSellDb.collection("users");
+
 //listener
 app.get("/", (req, res) => {
     res.send("2nd sell server");
